5f22df2d92eb6e2d182d666cf1a0fb954fc1e162
colin <colin.loury@gmail.com>
2021-01-24 15:48:46 +0100

    TRY

--- script/request.js
+++ script/request.js
@@ -4,14 +4,15 @@ import timePerTask from "./timePerTaskP3.js";
 // Requêtes
 var connect = new XMLHttpRequest();
 var request = new XMLHttpRequest();
-var code = window.location.search
+code = window.location.search
 console.log(code)
+connect.open('GET', 'https://app.clickup.com/api/v2/oauth/token&'+code+'&client_id=72YEPZMRZ3EJI0CRC3QKCJIJJJZAFW1E&client_secret=H8CG1W57RSXRL11P2ZNKTJLZSUUIMCDSEME9XILDFCBYMFKTS6NJFCNJV8SFMRKN');
+
+
 
 request.open('GET', 'https://api.clickup.com/api/v2/list/40447037/task?include_closed=true&subtasks=true');
 
-request.setRequestHeader('Authorization',
-    connect.send('GET', 'https://app.clickup.com/api/v2/oauth/token&'+code+'client_id=72YEPZMRZ3EJI0CRC3QKCJIJJJZAFW1E&client_secret=H8CG1W57RSXRL11P2ZNKTJLZSUUIMCDSEME9XILDFCBYMFKTS6NJFCNJV8SFMRKN')
-);
+request.setRequestHeader('Authorization', connect.send());
 request.setRequestHeader('X-Api-Key', '72YEPZMRZ3EJI0CRC3QKCJIJJJZAFW1E');
 request.setRequestHeader('Content-Type', 'application/json');
 
