2221f39076c100bfc3292919d0f3703ad15590c0
colin <colin.loury@gmail.com>
2021-01-24 14:52:05 +0100

    CHANGE authorization

--- script/request.js
+++ script/request.js
@@ -7,6 +7,7 @@ var request = new XMLHttpRequest();
 request.open('GET', 'https://api.clickup.com/api/v2/list/40447037/task?include_closed=true&subtasks=true');
 
 request.setRequestHeader('Authorization', '4594734_aa5ae1ce9bab31dfaa0ff7864860c49a0b86c1aa');
+request.setRequestHeader('X-Api-Key', '72YEPZMRZ3EJI0CRC3QKCJIJJJZAFW1E');
 request.setRequestHeader('Content-Type', 'application/json');
 
 request.onload = function () {
