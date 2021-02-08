1b1082c5b4ecfccc4fd0563f78d3a04bd2afcdbb
colin <colin.loury@gmail.com>
2021-02-08 17:28:24 +0100

    Travail sur la mise en place sur serveur

--- index.php
+++ index.php
@@ -1,15 +1,3 @@
-<!doctype html>
-<html lang="fr">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-    <link href="./style.css" rel="stylesheet">
-    <script src=".\node_modules\chart.js\dist\Chart.min.js"></script>
-    <title>Dashboard</title>
-  </head>
-  <body>
-  
 <?php
 $ch = curl_init();
 
@@ -24,6 +12,18 @@ curl_close($ch);
 
 var_dump($response);
 ?>
+
+<!doctype html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link href="./style.css" rel="stylesheet">
+    <script src=".\node_modules\chart.js\dist\Chart.min.js"></script>
+    <title>Dashboard</title>
+  </head>
+  <body>
   </body>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 </html>
