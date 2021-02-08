4c4e32a7664ef92efb6a09d494ca42442d83c89e
colin <colin.loury@gmail.com>
2021-02-08 16:03:45 +0100

    Travail sur la mise en place sur serveur

--- script/redirect.js
+++ script/redirect.js
@@ -1,7 +1,8 @@
 // Requêtes
 var request = new XMLHttpRequest();
 
-var code = location.search
+var paramet = location.search
+code = paramet.substring(1, paramet.length)
 console.log(code)
 
 request.open('POST', 'https://api.clickup.com/api/v2/oauth/token?client_id=0F7Q1WCJ0XCE24AGHWVU3R6Q5RJ0JPZQ&client_secret=UTZV6HUMS8BF8E246R9YXYTD8M1AU0M0WJNWSQ9MUUI18ZZU8WMFNC1JD1PRWKLB&'+code);
