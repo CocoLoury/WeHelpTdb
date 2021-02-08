83006ea2b7d1800d31b5d94bc768cabd2b1fe8c2
colin <colin.loury@gmail.com>
2021-02-08 20:05:36 +0100

    Travail sur la mise en place sur serveur

--- script/request.js
+++ script/request.js
@@ -9,7 +9,7 @@ var request = new XMLHttpRequest();
 
 request.open('GET', 'https://api.clickup.com/api/v2/list/40447037/task?include_closed=true&subtasks=true');
 
-request.setRequestHeader('Authorization', token);
+request.setRequestHeader('Authorization', "'"+token+"'");
 request.setRequestHeader('Content-Type', 'application/json');
 
 request.onload = function () {
