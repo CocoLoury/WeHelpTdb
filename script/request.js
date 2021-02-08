572e549d9aa52ae0f77875ccc5b09f89163747b0
colin <colin.loury@gmail.com>
2021-02-08 18:59:01 +0100

    Travail sur la mise en place sur serveur

--- script/request.js
+++ script/request.js
@@ -5,21 +5,23 @@ import requestTasks from "./coutTache.js";
 // Requêtes
 var request = new XMLHttpRequest();
 
-request.open('GET', 'https://api.clickup.com/api/v2/list/40447037/task?include_closed=true&subtasks=true');
+if(localStorage.getItem('access_token')) {
+    request.open('GET', 'https://api.clickup.com/api/v2/list/40447037/task?include_closed=true&subtasks=true');
 
-request.setRequestHeader('Authorization', 'basic '+localStorage.getItem('access_token'));
-request.setRequestHeader('Content-Type', 'application/json');
+    request.setRequestHeader('Authorization', localStorage.getItem('access_token'));
+    request.setRequestHeader('Content-Type', 'application/json');
 
-request.onload = function () {
-    if (request.status >= 200 && request.status < 400) {
-        var data = JSON.parse(this.response)
-        var tasks = data.tasks
-        var size = tasks.length
-        timePerTask(tasks, size)
-        nbStatusTasks(tasks, size)
-        requestTasks(tasks, size)
-    } else {
-        console.log('error')
-    }
-};
-request.send();
+    request.onload = function () {
+        if (request.status >= 200 && request.status < 400) {
+            var data = JSON.parse(this.response)
+            var tasks = data.tasks
+            var size = tasks.length
+            timePerTask(tasks, size)
+            nbStatusTasks(tasks, size)
+            requestTasks(tasks, size)
+        } else {
+            console.log('error')
+        }
+    };
+    request.send();   
+}
