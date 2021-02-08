cb4112a71f4faba720d16426d272bceb78630626
colin <colin.loury@gmail.com>
2021-02-08 16:33:21 +0100

    Travail sur la mise en place sur serveur

--- script/redirect.js
+++ script/redirect.js
@@ -6,6 +6,7 @@ code = paramet.substring(1, paramet.length)
 console.log(code)
 
 request.open('POST', 'https://api.clickup.com/api/v2/oauth/token?client_id=0F7Q1WCJ0XCE24AGHWVU3R6Q5RJ0JPZQ&client_secret=UTZV6HUMS8BF8E246R9YXYTD8M1AU0M0WJNWSQ9MUUI18ZZU8WMFNC1JD1PRWKLB&'+code+'&redirect_uri=https://wehelptdb.herokuapp.com/p3.php');
+request.setRequestHeader('Content-Type', 'application/json');
 request.onreadystatechange = function () {
     if (this.readyState === 4) {
       console.log('Status:', this.status);
