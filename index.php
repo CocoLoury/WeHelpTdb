abff79764a247b992744de7f44dac69b65344c2e
colin <colin.loury@gmail.com>
2021-02-08 17:33:36 +0100

    Travail sur la mise en place sur serveur

--- index.php
+++ index.php
@@ -9,10 +9,8 @@ curl_setopt($ch, CURLOPT_POST, TRUE);
 
 $response = curl_exec($ch);
 curl_close($ch);
-
 var_dump($response);
 ?>
-
 <!doctype html>
 <html lang="fr">
   <head>
@@ -27,3 +25,7 @@ var_dump($response);
   </body>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 </html>
+
+<script>
+    localStorage.setItem(<?php $response ?>);
+</script>
